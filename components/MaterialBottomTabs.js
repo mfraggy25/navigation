514fc7b1f748f8f53f9135f99ae8140666a05f96
mfraggy25 <mfraggy25@users.noreply.github.com>
2020-05-28 21:00:32 -0400

    add bottom tab icons

--- components/MaterialBottomTabs.js
+++ components/MaterialBottomTabs.js
@@ -4,20 +4,74 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Home from "./Home";
 import Profile from "./Profile";
 import Contact from "./Contact";
 import Chat from "./Chat";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Contact" component={Contact} />
-      <Tab.Screen name="Chat" component={Chat} />
+    <Tab.Navigator
+      initialRouteName="Profile"
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: "#694fad" }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarColor: "red",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Contact"
+        component={Contact}
+        options={{
+          tabBarLabel: "Contact",
+          tabBarColor: "blue",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="contact-mail"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarLabel: "Chat Room",
+          tabBarColor: "brown",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="chat-outline"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
